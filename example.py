9154210fc499ca2d871699924031f9057f120733
Nagarjun P <53596669+NagarjunP23@users.noreply.github.com>
2020-09-29 17:40:47 +0530

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,7 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository.")
 git_opeation()
+print("doing again for qwiklab")
 =======
 
 >>>>>>> fa94b9997c65bcfd5ebb1f9af5c2f0ed445f2678
